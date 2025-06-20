93e0e84e8da153d786edb1e3167fc9457d0c7454
seunga418 <seungab77@gmail.com>
2025-06-20 10:54:04 +0900

    백준 1316번 문제
    백준 1316번 문제 풀었습니다

---
+++ codeup/1316.py
@@ -0,0 +1,21 @@
+N = int(input())
+count = 0
+
+for _ in range(N):
+    word = input()
+    seen = set()
+    prev_char = ''
+    is_group = True
+
+    for char in word:
+        if char != prev_char:
+            if char in seen:
+                is_group = False
+                break
+            seen.add(char)
+        prev_char = char
+
+    if is_group:
+        count += 1
+
+print(count)
