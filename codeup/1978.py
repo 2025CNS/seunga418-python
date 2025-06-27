b2a22f73f5b546455aa9a6469c5e5d9276eaea0d
seunga418 <seungab77@gmail.com>
2025-06-27 18:45:15 +0900

    백준 1978번 문제
    백준 1978번 문제 풀었습니다

---
+++ codeup/1978.py
@@ -0,0 +1,14 @@
+def a(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+N = int(input())
+numbers = list(map(int, input().split()))
+count = 0
+for num in numbers:
+    if a(num):
+        count += 1
+print(count)
