6e24850cbc90f2d235af634f70e4ad3aa3d690d4
seunga418 <seungab77@gmail.com>
2025-07-06 00:13:35 +0900

    백준 1541번 문제
    백준 1541번 문제 풀었습니다

---
+++ codeup/1541.py
@@ -0,0 +1,7 @@
+expression = input().strip()
+parts = expression.split('-')
+initial = sum(map(int, parts[0].split('+')))
+rest = 0
+for part in parts[1:]:
+    rest += sum(map(int, part.split('+')))
+print(initial - rest)
